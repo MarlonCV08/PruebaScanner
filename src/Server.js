bc4991e2c663951a26a0d206fd85084c41111b1f
Marlon <marlonstiven0208@gmail.com>
2024-10-09 21:54:13 -0500

    ...

--- src/Server.js
+++ src/Server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-    origin: 'https://prueba-scanner.vercel.app/',
+    origin: 'http://localhost:5173',
     methods: ['GET', 'POST'], 
 };
 
