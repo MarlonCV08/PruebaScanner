af03f709996f08366c065b372e3dcc1fb2f676c8
Marlon <marlonstiven0208@gmail.com>
2024-10-09 21:41:39 -0500

    ..

--- src/Server.js
+++ src/Server.js
@@ -5,8 +5,13 @@ import cors from 'cors'; // Para manejar CORS si es necesario
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'], 
+};
+
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 // Ruta para guardar datos
